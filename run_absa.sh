a6203e076769a53bda60f8e87c9ebc815f7570df
wcy_scnu_nlp <91064816+swaggy66@users.noreply.github.com>
2025-02-19 00:03:33 +0800

    Update run_absa.sh

--- run_absa.sh
+++ run_absa.sh
@@ -18,20 +18,20 @@
 #             --eval_begin_end 1500-2000 
 # "
 # mbert_path=../trained-transformers/bert-multi-cased
-mbert_path=/data/wuchengyan/XABSA-master/mBERT
+mbert_path=./mBERT
 # xlmr_path=../trained-transformers/xlmr-base
-xlmr_path=/data/wuchengyan/XABSA-master/XLM-R
+xlmr_path=./XLM-R
 
-python /data/wuchengyan/XABSA-master/main.py --tfm_type xlmr \
+python main.py --tfm_type xlmr \
             --exp_type supervised \
             --model_name_or_path $mbert_path \
-            --data_dir ./data1 \
+            --data_dir ./data \
             --src_lang fr \
             --tgt_lang fr \
             --do_train \
             --do_eval \
             --ignore_cached_data \
-            --per_gpu_train_batch_size 8 \
+            --per_gpu_train_batch_size 16 \
             --per_gpu_eval_batch_size 8 \
             --learning_rate 2e-5 \
             --tagging_schema BIEOS \
